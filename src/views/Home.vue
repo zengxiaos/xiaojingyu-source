5e65dcc053833647359e73099f6baf22ca50d511
zengxiaosen <2272652982@qq.com>
2022-09-01 13:43:13 +0800

    完成Home的样式并初始化DOC

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>Z-UI</h1>
       <h2>一个自用的UI 框架</h2>
       <p class="actions">
-        <a>GitHub</a>
-        <a>开始</a>
+        <a href="//">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -17,3 +17,26 @@ export default {
   components: {TopNav}
 }
 </script>
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+</style>
