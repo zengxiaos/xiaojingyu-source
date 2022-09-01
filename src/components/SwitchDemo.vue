2ccbf440fd15e8b152979fba467a71073827209f
zengxiaosen <2272652982@qq.com>
2022-09-01 19:55:49 +0800

    button hover之后span右滚

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,3 +1,12 @@
 <template>
-  <div>SwitchDemo</div>
+  <div>
+    <Switch/>
+  </div>
 </template>
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+export default {
+  components:{Switch}
+}
+
+</script>
