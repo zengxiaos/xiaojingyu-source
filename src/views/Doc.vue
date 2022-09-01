f789a11aa704ff93db73ee30e36d516abae48212
zengxiaosen <2272652982@qq.com>
2022-09-01 13:50:31 +0800

    Doc的main

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-   <TopNav/>
+    <TopNav/>
     <div class="content">
-      <aside >
+      <aside>
         <h2>组件列表</h2>
         <ol>
           <li>
@@ -10,15 +10,21 @@
               Switch 组件
             </router-link>
           </li>
-          <li> <router-link to="/doc/button">
-            Button 组件
-          </router-link></li>
-          <li> <router-link to="/doc/dialog">
-            Dialog 组件
-          </router-link></li>
-          <li> <router-link to="/doc/tabs">
-            Tabs 组件
-          </router-link></li>
+          <li>
+            <router-link to="/doc/button">
+              Button 组件
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">
+              Dialog 组件
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">
+              Tabs 组件
+            </router-link>
+          </li>
         </ol>
       </aside>
       <main>主内容</main>
@@ -26,8 +32,31 @@
   </div>
 </template>
 <script lang="ts">
-import TopNav from "../components/TopNav.vue";
+import TopNav from '../components/TopNav.vue';
+
 export default {
   components: {TopNav}
-}
+};
 </script>
+
+<style lang="scss" scoped>
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 53px;
+  left: 0;
+
+> h2 {
+  margin-bottom: 4px;
+}
+
+> ol {
+
+> li {
+  padding: 4px 0;
+}
+
+}
+}</style>
