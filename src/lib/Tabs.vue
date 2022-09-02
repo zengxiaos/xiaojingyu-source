f3399557178729adabf6cf41806d62808abb0830
zengxiaosen <2272652982@qq.com>
2022-09-02 21:07:33 +0800

    利用slot和component获取内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
-    Tabs组件
+    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
+    <component v-for="(c,index) in defaults" :is="c" :key="index"/>
   </div>
 </template>
+
+<script lang="ts">
+import Tab from './Tab.vue'
+export default {
+  components: {},
+
+  setup(props,context){
+   const defaults=context.slots.default()
+    defaults.forEach(tag=>{if(tag.type!==Tab){
+    throw new Error('Tabs子标签必须是Tab')}
+    })
+    const titles=defaults.map((tag)=>{
+      return tag.props.title
+    })
+    return{defaults,titles}
+  }
+}
+</script>
