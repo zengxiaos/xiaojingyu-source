8c01673d6765c347e10d0ab7cee2bcd14aa37d98
zengxiaosen <2272652982@qq.com>
2022-09-02 21:49:01 +0800

    遇到vue3 bug

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,24 +1,70 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav">
+      <div :class="{selected:t===selected}" class="gulu-tabs-nav-item"
+           v-for="(t,index) in titles" :key="index" @click="select(t)">{{t}}</div>
+    </div>
+    <div class="gulu-tabs-content">
+      <component class="gulu-tabs-content-item"  :is="current"  />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue'
+import {computed} from 'vue';
 export default {
-  components: {},
-
-  setup(props,context){
-   const defaults=context.slots.default()
-    defaults.forEach(tag=>{if(tag.type!==Tab){
-    throw new Error('Tabs子标签必须是Tab')}
+  props:{selected:{type:String}},
+  setup(props, context) {
+    const defaults = context.slots.default()
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs 子标签必须是 Tab')
+      }
     })
-    const titles=defaults.map((tag)=>{
+    const current=computed(()=>{
+     return defaults.filter((tag)=>{
+     return  tag.props.title===props.selected
+    })[0]})
+    const titles = defaults.map((tag) => {
       return tag.props.title
     })
-    return{defaults,titles}
+    const select=(title:string)=>{
+      context.emit('update:selected',title)
+    }
+    return {
+      defaults,
+      titles,
+      current,
+      select
+    }
   }
 }
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
